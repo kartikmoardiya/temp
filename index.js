3fe6f2cc3b46ad9d996c75a128de95b990e32839
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2025-06-17 22:29:43 +0530

    'third'

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 const db = require('./Database/db');
 const router = express.Router();
 const model = require('./Models/user')
+const cors = require("cors");
+
 const PORT = process.env.PORT || 3000;
 
 
 
 const user = require('./Routes/users')
+
+app.use(cors());
 app.use(express.json());
 app.use('/user',user); 
 
